bc22758497a38d4c55a0d5d107f877d1bcbfd18a
Mariano Gardellini <m.gardellini@us.nature.com>
2013-10-12 18:37:02 -0400

    changed yum repo url

--- site.pp
+++ site.pp
@@ -1,13 +1,6 @@
 yumrepo { "EPELrepo":
-  baseurl  => "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm",
+  baseurl  => "http://rpms.famillecollet.com/enterprise/6/remi/mirror",
   descr    => "EPEL",
-  enabled  => 1,
-  gpgcheck => 0,
-}
-
-yumrepo { "REMIrepo":
-  baseurl  => "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm",
-  descr    => "REMI",
   enabled  => 1,
   gpgcheck => 0,
 }
