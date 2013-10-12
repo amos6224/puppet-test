ce649f39cef269a41e28b91609c401ae399fd46f
Mariano Gardellini <m.gardellini@us.nature.com>
2013-10-12 18:28:38 -0400

    added yum repos

--- site.pp
+++ site.pp
@@ -1,1 +1,15 @@
+yumrepo { "EPELrepo":
+  baseurl  => "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm",
+  descr    => "EPEL",
+  enabled  => 1,
+  gpgcheck => 0,
+}
+
+yumrepo { "REMIrepo":
+  baseurl  => "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm",
+  descr    => "REMI",
+  enabled  => 1,
+  gpgcheck => 0,
+}
+
 import 'nodes.pp'
